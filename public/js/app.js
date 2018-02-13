c7dea60137a6e04623c7811dd152cc7f3bcb45eb
Jennifercarmen <jennifercarmens@gmail.com>
2018-02-13 17:46:29 -0500

    v1 proyecto usando servidor y ajax

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+});
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=dfcd365f4d354fd4abb3e5308452082b`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+function handleError() {
+  console.log('Se ha presentado un error');
+}
+
+function addNews() {
+  const data = JSON.parse(this.responseText);
+  const article = data.response.docs[0];
+  const title=article.headline.main;
+  const snippet=article.snippet;
+  let li=document.createElement('li');
+  li.className='articleClass';
+  li.innerText=snippet;
+  responseContainer.appendChild(li);
+  console.log(data);
+}
